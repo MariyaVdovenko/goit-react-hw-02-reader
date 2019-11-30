d4e419b2efc25668edc8ccbeaa25ac3d90d23643
MariyaVdovenko <m.vdovenko@ukr.net>
2019-12-01 00:55:16 +0200

    Updates

---
+++ precache-manifest.d2601e05b79119826ea42a2305139dba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f202882bc7a2e535810b854e2947e24",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "5cb168ec48f96dd7a25d",
+    "url": "/goit-react-hw-02-reader/static/css/main.bc00c974.chunk.css"
+  },
+  {
+    "revision": "b40bf4929657dbff27fc",
+    "url": "/goit-react-hw-02-reader/static/js/2.7066c695.chunk.js"
+  },
+  {
+    "revision": "5cb168ec48f96dd7a25d",
+    "url": "/goit-react-hw-02-reader/static/js/main.265b3db4.chunk.js"
+  },
+  {
+    "revision": "299f48f5aa6019a15bd5",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.ef9e362e.js"
+  }
+]);
