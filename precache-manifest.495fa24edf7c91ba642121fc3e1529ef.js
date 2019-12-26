a18575e75b6b5a5dd5acb7b74df3c3ff7098b9cf
MariyaVdovenko <m.vdovenko@ukr.net>
2019-12-26 15:59:25 +0200

    Updates

--- precache-manifest.495fa24edf7c91ba642121fc3e1529ef.js
+++ precache-manifest.495fa24edf7c91ba642121fc3e1529ef.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f202882bc7a2e535810b854e2947e24",
+    "revision": "525a5f6b44b5fa9a5df9f432c0df834d",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "5cb168ec48f96dd7a25d",
+    "revision": "dd61ba47a226edad86d2",
     "url": "/goit-react-hw-02-reader/static/css/main.bc00c974.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-reader/static/js/2.7066c695.chunk.js"
   },
   {
-    "revision": "5cb168ec48f96dd7a25d",
-    "url": "/goit-react-hw-02-reader/static/js/main.265b3db4.chunk.js"
+    "revision": "dd61ba47a226edad86d2",
+    "url": "/goit-react-hw-02-reader/static/js/main.d87b7c0d.chunk.js"
   },
   {
     "revision": "299f48f5aa6019a15bd5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-reader/precache-manifest.d2601e05b79119826ea42a2305139dba.js"
+  "/goit-react-hw-02-reader/precache-manifest.495fa24edf7c91ba642121fc3e1529ef.js"
 );
 
 self.addEventListener('message', (event) => {
